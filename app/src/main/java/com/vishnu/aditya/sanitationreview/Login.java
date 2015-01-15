70a5f9396689c536b3d9e2864f5b4b24ba82e3ab
Aditya Prasad <adityaprasadiskool@gmail.com>
2015-01-15 12:14:09 +0530

    Saving progress
    In the middle of getting login activity working
    
    Signed-off-by: Aditya Prasad <adityaprasadiskool@gmail.com>

--- app/src/main/java/com/vishnu/aditya/sanitationreview/Login.java
+++ app/src/main/java/com/vishnu/aditya/sanitationreview/Login.java
@@ -1,9 +1,13 @@
 package com.vishnu.aditya.sanitationreview;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Login extends ActionBarActivity {
@@ -12,9 +16,38 @@ public class Login extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // set a listener for the EditText
+        ((EditText)this.findViewById(R.id.empId)).setOnKeyListener(new View.OnKeyListener() {
+
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    // if the enter key was pressed, then try logging in
+                    attemptLogin(v);
+                    return true;
+                }
+
+                return false;
+            }
+        });
     }
 
+    public void attemptLogin(View v){
+        // Take the employee ID entered by the user
+        EditText empId = (EditText)findViewById(R.id.empId);
+        String employeeID = empId.getText().toString();
 
+        // check if user has not entered any valid string.
+        if(employeeID.trim().length() == 0){
+            // alert the user to enter the ID
+            Toast.makeText(this,"Please enter a Employee ID",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // This sends the employee ID to the server for verification
+        new EmployeeSender(this).execute(employeeID);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
